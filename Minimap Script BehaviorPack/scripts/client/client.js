aed82234929c55efbe77ce803a90d9cd451edb70
Su-Yong <simssy2205@gmail.com>
2019-10-20 17:46:52 +0900

    upload initial files

---
+++ Minimap Script BehaviorPack/scripts/client/client.js
@@ -0,0 +1,93 @@
+const DEBUG = true
+
+const clientSystem = client.registerSystem(0, 0)
+
+clientSystem.initialize = function () {
+  this.listenForEvent('minimap:update_block', event => this.onUpdateBlock(event.data.blocks, event.data.yaw))
+  this.listenForEvent('minimap:update_coord', event => this.onUpdateCoord(event.data.x, event.data.y, event.data.z))
+
+  this.listenForEvent('minecraft:ui_event', event => this.onUIEvent(JSON.parse(event.data).id, JSON.parse(event.data).data))
+  this.listenForEvent('minecraft:client_entered_world', event => this.onWorldLoaded(event.data.player))
+
+
+  if(DEBUG) {
+    let debug_config = this.createEventData('minecraft:script_logger_config')
+    debug_config.data.log_errors = true
+    debug_config.data.log_warnings = true
+    debug_config.data.log_information = true
+
+    this.broadcastEvent('minecraft:script_logger_config', debug_config)
+  }
+}
+
+// Hooks --------------------------------------------------
+
+clientSystem.update = function () {
+
+}
+
+clientSystem.onUpdateBlock = function (blocks, yaw) {
+  let eventData = this.createEventData('minecraft:send_ui_event')
+  eventData.data.eventIdentifier = 'update_minimap'
+  
+  let data = {
+    data_str: blocks,
+    yaw: yaw
+  }
+  eventData.data.data = JSON.stringify(data)
+
+  this.broadcastEvent('minecraft:send_ui_event', eventData)
+}
+
+clientSystem.onUpdateCoord = function (x, y, z) {
+  let eventData = this.createEventData('minecraft:send_ui_event')
+  eventData.data.eventIdentifier = 'update_coord'
+  
+  let data = {
+    x: (x > 0 ? '+' : '') + Math.round(x),
+    y: Math.round(y),
+    z:(z > 0 ? '+' : '') +  Math.round(z)
+  }
+  eventData.data.data = JSON.stringify(data)
+
+  this.broadcastEvent('minecraft:send_ui_event', eventData)
+}
+
+clientSystem.onWorldLoaded = function (player) {
+  let loadData = this.createEventData('minecraft:load_ui')
+  loadData.data.path = 'minimap.html'
+  loadData.data.options = {
+    always_accepts_input: false,
+
+    render_game_behind: true,
+    absorbs_input: false,
+    is_showing_menu: false,
+    should_steal_mouse: true,
+    force_render_below: true,
+
+    render_only_when_topmost: false,
+  }
+
+  this.broadcastEvent('minecraft:load_ui', loadData)
+  this.showMessage('Minimap Add-ons by SuYong')
+}
+
+clientSystem.onDestryBlock = function (event) {
+
+}
+
+clientSystem.onUIEvent = function(id, data) {
+  switch(id) {
+    case 'debug_show_message':
+      this.showMessage(data.message)
+      break
+  }
+}
+
+// Custom Funcitons --------------------------------------------------
+
+clientSystem.showMessage = function (message) {
+  let eventData = this.createEventData('minecraft:display_chat_event')
+  eventData.data.message = message
+  this.broadcastEvent('minecraft:display_chat_event', eventData)
+}
---
+++ Minimap Script BehaviorPack/scripts/server/server.js
@@ -0,0 +1,160 @@
+const DEBUG = true
+
+const serverSystem = server.registerSystem(0, 0)
+
+serverSystem.initialize = function () {
+  this.registerEventData('minimap:update_block', { blocks: [] })
+  this.registerEventData('minimap:update_coord', { x: 0, y: 0, z: 0 })
+  this.registerEventData('minimap:entity_created', { position: [], entity: null })
+
+  this.listenForEvent('minecraft:entity_created', event => this.onEntityCreated(event.data.entity))
+  this.listenForEvent('minecraft:entity_tick', event => this.onEntityTicked(event.data.entity))
+
+  if (DEBUG) {
+    let debug_config = this.createEventData('minecraft:script_logger_config')
+    debug_config.data.log_errors = true
+    debug_config.data.log_warnings = true
+    debug_config.data.log_information = true
+
+    this.broadcastEvent('minecraft:script_logger_config', debug_config)
+  }
+}
+
+// Define variables --------------------------------------------------
+
+let cool = 100
+let players = []
+let old_pos = []
+
+let blocks = ''
+let tickingArea = null
+
+// algorithms --------------------------------------------------
+
+function getBlocks(tickingArea, x, y, z) {
+  let size = 10
+  let ysize = 4
+  let result = ''
+  for (let xi = size; xi > -size; xi--) {
+    for (let zi = -size; zi < size; zi++) {
+      yloop: for (let yi = ysize; yi > -ysize; yi--) {
+        let block = serverSystem.getBlock(tickingArea, x + xi, y + yi, z + zi).__identifier__
+        if(block != 'minecraft:air') {
+          result += colorize(block)
+          break yloop
+        }
+      }
+    }
+  }
+
+  return result
+}
+
+/*
+rotation y
+-180 ~ 180
+       +z
+       0
++x -90   +90
+    +-180
+*/
+
+// Hooks --------------------------------------------------
+
+serverSystem.update = function () {
+  if (players.length > 0) {
+    let pos = this.getComponent(players[0], 'minecraft:position').data
+    if (tickingArea == null) {
+      tickingArea = this.getComponent(players[0], 'minecraft:tick_world').data.ticking_area
+    }
+
+    if (cool <= 0) {
+      let event = this.createEventData('minimap:update_block')
+      //event.data.blocks = getBlocks(tickingArea, pos.x, pos.y - 1, pos.z)
+      event.data.blocks = normalize(this.getBlocks(tickingArea, pos.x - 10, pos.y - 1, pos.z - 10, pos.x + 10, pos.y - 1, pos.z + 10))
+      event.data.yaw = this.getComponent(players[0], 'minecraft:rotation').data.y
+
+      this.broadcastEvent('minimap:update_block', event)
+
+      let eventCoord = this.createEventData('minimap:update_coord')
+      
+      eventCoord.data.x = pos.x
+      eventCoord.data.y = pos.y
+      eventCoord.data.z = pos.z
+      
+      this.broadcastEvent('minimap:update_coord', eventCoord)
+      cool = 4
+    }
+
+    cool--
+  }
+}
+
+/**
+ * entity ticked
+ * @param entity {Entity}
+ */
+serverSystem.onEntityTicked = function (entity) {
+  
+}
+
+/**
+ * entity created
+ * @param entity {Entity} 
+ */
+serverSystem.onEntityCreated = function (entity) {
+  if (entity.__identifier__ == 'minecraft:player') {
+    players.push(entity)
+  }
+}
+
+// Custom Funcitons --------------------------------------------------
+
+/**
+ * show message
+ * @param message {String} 
+ */
+serverSystem.showMessage = function (message) {
+  let eventData = this.createEventData('minecraft:display_chat_event')
+  eventData.data.message = message
+  this.broadcastEvent('minecraft:display_chat_event', eventData)
+}
+
+/**
+ * translate from block array to string
+ * @param blockArray {Array}
+ * @return colorCodes {String}
+ */
+function normalize(array) {
+  let result = ''
+
+  for (let x in array) {
+    for (let z in array[x][0]) {
+      result += colorize(array[20 - x][0][z].__identifier__)
+    }
+  }
+
+  return result
+}
+
+/**
+ * change identifier to number
+ * @param blockIdentifier {String}
+ * @return colorCode {Number}
+ */
+function colorize(name) {
+  switch (name) {
+    case 'minecraft:grass':
+      return 0//'#7FB238'
+    case 'minecraft:stone': case 'minecraft:cobblestone':
+      return 1//'#707070'
+    case 'minecraft:sand':
+      return 2//'#F7E9A3'
+    case 'minecraft:dirt':
+      return 3//'#976D4D'
+    case 'minecraft:water':
+      return 4//'#4040FF'
+    default:
+      return 5//'#FFFFFF'
+  }
+}
